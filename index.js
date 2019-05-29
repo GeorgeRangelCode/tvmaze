e2ec633cb4032cb412a69b3e83f3c7ea6bda10c3
georgerangelcode <georgerangelcode@gmail.com>
2019-05-29 16:12:06 -0500

    Web Site Api tvmaze

---
+++ index.js
@@ -0,0 +1,23 @@
+const API = "http://api.tvmaze.com/shows";
+const element = document.getElementById("tvshows");
+
+function getData(urlApi) {
+  fetch(urlApi).then(response => {
+    response.json().then(data => {
+      let output = data
+        .slice(0, 16)
+        .map(function(show) {
+          return `
+          <article class="show-card">
+            <img src="${show.image.medium}" alt="${show.name}" />
+            <h2>${show.name}</h2>
+          </article>
+          `;
+        })
+        .join("");
+      element.innerHTML = output;
+    });
+  });
+}
+
+getData(API);
